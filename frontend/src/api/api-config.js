80e85dbb002d403a2a9fb5927967d4878f7cd22e
zlzkzlgh <zlzkzlgh@gmail.com>
2024-12-31 17:20:45 +0900

    Revert "Merge branch 'HJW2' of https://github.com/ChoHanEarl/Team-MovieStar into HJW2"
    This reverts commit 9d052e8a459d61dcfaf329fd6266be617dbb418b, reversing
    changes made to 3c14c924fa6ea0ac394edd8d26fcbb4a1240d9db.

--- frontend/src/api/api-config.js
+++ frontend/src/api/api-config.js
@@ -3,7 +3,6 @@ const hostname = window && window.location && window.location.hostname;
 
 if(hostname === "localhost"){
     backendHost = "http://localhost:5000";
-
 }else {
     backendHost = "https://api.moviestar-hjw.site";
   }
