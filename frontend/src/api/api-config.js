e0cd8b0472b1c09907d34d61b4746be9d8bc5b31
zlzkzlgh <zlzkzlgh@gmail.com>
2025-01-03 09:41:57 +0900

    change
    change

--- frontend/src/api/api-config.js
+++ frontend/src/api/api-config.js
@@ -3,9 +3,9 @@ const hostname = window && window.location && window.location.hostname;
 
 if(hostname === "localhost"){
     backendHost = "http://localhost:5000";
-} else {
-    backendHost = "https://moviestar-moon.site/api";
-}
+}else {
+    backendHost = "https://api.moviestar-hjw.site";
+  }
 
 
 export const API_BASE_URL = `${backendHost}`
